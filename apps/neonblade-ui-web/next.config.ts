03878e2f3026343c0bc6712cbd4838d68343c543
vprix21 <vkalwale21@gmail.com>
2026-04-11 23:41:19 +0530

    removed allowed domains from next config

--- apps/neonblade-ui-web/next.config.ts
+++ apps/neonblade-ui-web/next.config.ts
@@ -2,12 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: [
-    "192.168.1.8",
-    "192.168.1.2",
-    "192.168.1.3",
-    "192.168.1.9",
-  ],
+  allowedDevOrigins: [],
 };
 
 export default nextConfig;
